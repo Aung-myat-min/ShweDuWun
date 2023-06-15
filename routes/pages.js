cf14e1d34d467079a28ad3ef761ea565ecfa00d1
Aung Myat Min <koaungmyatmin0@gmail.com>
2023-06-15 13:52:39 +0630

    add some

--- routes/pages.js
+++ routes/pages.js
@@ -6,4 +6,15 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../views/homepage.html"));
 })
 
+router.get('/contactus', (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/contactus.html"));
+})
+
+router.get('/store', (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/store.html"));
+})
+
+router.get('/items', (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/items.html"));
+})
 module.exports = router;
