c2a7c34d4519fda23795bd89e30108794e9ef8d9
Aung Myat Min <koaungmyatmin0@gmail.com>
2023-06-18 11:23:34 +0630

    matching with vercel

--- server.js
+++ server.js
@@ -9,5 +9,29 @@ app.use("/css", express.static(__dirname+"/public/css"));
 app.use("/imgs", express.static(__dirname+"/public/imgs"));
 app.use("/", require('./routes/pages'));
 
+//routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/homepage.html"));
+})
+
+app.get('/contactus', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/contactus.html"));
+})
+
+app.get('/store', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/store.html"));
+})
+
+app.get('/items', (req, res) => {
+    res.render("items");
+})
+
+app.get('/terms', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/terms.html"));
+})
+//routes
+
 app.listen(port);
 console.log("\n===== *** WEB is running *** =====\n")
+
+module.exports = app;
