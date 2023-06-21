18601a66305f28930fbf97137e2a616e297b48f1
Aung Myat Min <koaungmyatmin0@gmail.com>
2023-06-21 10:46:11 +0630

    updated backend and item.ejs

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const path = require('path');
 const port = 5001;
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const findRowById = (id) => {
+    return new Promise((resolve, reject) => {
+      fs.createReadStream(__dirname + '/public/item_details.csv')
+        .pipe(csv({ separator: '|' })) // Set the separator as '|'
+        .on('data', (data) => {
+          if (data.ID === id) {
+            resolve(data);
+            console.log(data)
+          }
+        })
+        .on('end', () => {
+          reject(new Error('Row not found'));
+        })
+        .on('error', (error) => {
+          reject(error);
+        });
+    });
+  };
+  
 
 const app = express();
 app.set("view engine", "ejs");
@@ -24,8 +46,56 @@ app.get('/store', (req, res) => {
 })
 
 app.get('/items', (req, res) => {
-    res.render("items");
+    res.redirect("/store");
 })
+
+const csvParserConfig = {
+    delimiter: '|',
+    headers: ['Item_id', 'Details', 'Value'],
+    skipLines: 1, // Skip the first line (header row)
+};
+  
+  
+app.get('/items/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('ID:', id);
+    const results = [];
+    console.log('URL:', req.url);
+    fs.createReadStream(__dirname + '/public/item_details.csv')
+      .pipe(csv(csvParserConfig)) // Pass the parser configuration
+      .on('data', (data) => {
+        console.log('Row:', data);
+        results.push(data);
+      })
+      .on('end', () => {
+        const row = results.find((row) => row.Item_id.startsWith(id)); // Compare substring
+        const rowString = JSON.stringify(row); // Convert row object to string
+        const cleanedString = rowString.replace(/["{}]/g, ''); // Remove unwanted characters
+        const rowList = cleanedString.split('|'); // Split the string back into a list using '|'
+
+        console.log(rowList);
+
+
+        console.log('Found Row:', row);
+        if (row) {
+          const dataToEJS = {
+            image: id + '.png',
+            price: rowList[2],
+            details: rowList[1],
+          };
+          res.render('items', dataToEJS);
+        } else {
+          console.error('Row not found');
+          res.render('error', { message: 'Item not found' });
+        }
+      })
+      .on('error', (error) => {
+        console.error(error);
+        res.render('error', { message: 'Error reading CSV file' });
+      });
+  });
+  
+   
 
 app.get('/delivery', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/delivery.html"));
